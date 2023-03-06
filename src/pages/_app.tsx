96b3104035162b1a12ec39bed80151cd2dc7045e
olejri <olejri@gmail.com>
2023-03-06 19:25:58 +0100

    Added Clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
 import { type AppType } from "next/app";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton
+} from "@clerk/nextjs";
 
 import { api } from "npm/utils/api";
 
 import "npm/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps} >
+      <SignedIn>
+        <UserButton />
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
