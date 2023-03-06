deed72837d020ba0c349b8606fa9760aaebcf067
olejri <olejri@gmail.com>
2023-03-06 19:50:14 +0100

    Added PlanetScale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,24 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "npm/utils/api";
+import { useUser } from '@clerk/nextjs'
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
 
   return (
     <>
       <Head>
         <title>Game Tracker</title>
-
       </Head>
       <main>
         <h1>Game Tracker</h1>
+        <h2>Welcome user: {user.firstName}</h2>
         {hello.data ? hello.data.greeting : "Loading tRPC query..."}
       </main>
     </>
