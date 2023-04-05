f44247f2696e3e0566424ab2452748babc7a649e
olejri <olejri@gmail.com>
2023-04-05 18:18:47 +0200

    removed arrow

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -106,9 +106,6 @@ export default function Dashboard(groupName: string) {
             className="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
             aria-hidden="true"
           >
-            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-            </svg>
           </span>
         </div>
       ))}
