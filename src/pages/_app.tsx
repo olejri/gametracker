dd9529493a98a505c0701dd81c0b7bb4ef24ab7d
olejri <olejri@gmail.com>
2023-04-25 21:05:35 +0200

    trying vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ import { GameGroupContextProvider } from "npm/context/GameGroupContext";
 import HasChosenGameGroup from "npm/components/HasChosenGameGroup";
 import NotChosenGameGroup from "npm/components/NotChosenGameGroup";
 import MainComponent from "npm/components/MainLayout";
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
@@ -49,6 +50,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           </div>
         </SignInButton>
       </SignedOut>
+      <Analytics/>
     </ClerkProvider>
   );
 };
