9b83f4ae33ddedffaf040316809f77a05ab275b9
olejri <olejri@gmail.com>
2023-11-21 22:21:12 +0100

    trying openai v2

--- src/components/GameSearch.tsx
+++ src/components/GameSearch.tsx
@@ -1,38 +1,9 @@
 import React, { useState } from "react";
-import { type OpenWithGameId } from "npm/components/Types";
-import { api } from "npm/utils/api";
-import { LoadingPage } from "npm/components/loading";
 import SearchBar from "npm/components/SearchBar";
 
-
 const GameSearch = () => {
-  const [baseGameId, setBaseGameId] = useState<string>();
   const [atlasGamesResult, setAtlasGamesResult] = useState<{text : {value: string}}>();
-  const { data: mechanics, isLoading: mechanicsIsLoading, error: mechanicsErrors } = api.game.getAllMechanics.useQuery();
-  const { data: categories, isLoading: categoriesIsLoading, error: categoriesError } = api.game.getAllCategories.useQuery();
-  const mutationSearch= api.game.searchForGameWithOpenai.useMutation(
-    {
-      onSuccess: (data) => {
-        setAtlasGamesResult(data);
-      }
-    }
-  )
-  const [modalOpen, setModalOpen] = useState<OpenWithGameId>({open: false, name: ""});
-  const { data: collections } = api.game.getAllGames.useQuery();
-  const ctx = api.useContext()
 
-  const mutation = api.game.addGame.useMutation({
-    onSuccess: () => {
-      void ctx.game.getAllGames.invalidate();
-    }
-  });
-
-  if (categoriesError || mechanicsErrors) return <p>There is an error.</p>;
-  if (!categories || !collections || mechanicsIsLoading || categoriesIsLoading || mutationSearch.isLoading) return (
-    <div className="flex grow">
-      <LoadingPage />
-    </div>
-  );
 
   if (atlasGamesResult === undefined) return (
     <div className="sm:w-3/12">
@@ -42,8 +13,12 @@ const GameSearch = () => {
 
   return (
     <>
-      <p className="text-xl font-bold mb-4">Search result</p>
-      {atlasGamesResult.text.value}
+      <div className="sm:w-3/12">
+        <SearchBar setAtlasGamesResult={setAtlasGamesResult} />
+      </div>
+      <div>
+        {atlasGamesResult.text.value}
+      </div>
     </>
   );
 };
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { api } from "npm/utils/api";
 import { LoadingPage } from "npm/components/loading";
-import SelectWithSearch from "npm/components/SelectWithSearch";
 
 
 const SearchBar = (props: {
@@ -9,18 +8,7 @@ const SearchBar = (props: {
 }) => {
   const setAtlasGamesResult = props.setAtlasGamesResult;
   const [searchName, setSearchName] = useState("");
-  const [mechanic, setMechanic] = useState<string>("");
-  const [category, setCategory] = useState<string>("");
-  const {
-    data: mechanics,
-    isLoading: mechanicsIsLoading,
-    error: mechanicsErrors
-  } = api.game.getAllMechanics.useQuery();
-  const {
-    data: categories,
-    isLoading: categoriesIsLoading,
-    error: categoriesError
-  } = api.game.getAllCategories.useQuery();
+
   const mutationSearch = api.game.searchForGameWithOpenai.useMutation(
     {
       onSuccess: (data) => {
@@ -29,8 +17,7 @@ const SearchBar = (props: {
     }
   );
 
-  if (categoriesError || mechanicsErrors) return <p>There is an error.</p>;
-  if (!categories || mechanicsIsLoading || categoriesIsLoading || mutationSearch.isLoading) return (
+  if (mutationSearch.isLoading) return (
     <div className="flex grow">
       <LoadingPage />
     </div>
@@ -41,14 +28,6 @@ const SearchBar = (props: {
       <h1 className="text-xl font-bold mb-4">Search after a game</h1>
       <input className="border border-gray-300 rounded py-2 px-4" type="text" placeholder="Name of the game"
              onChange={(e) => setSearchName(e.target.value)} />
-      <SelectWithSearch
-        items={mechanics.map((m) => {
-          return { id: m.id, name: m.name ?? "Unnamed mechanic" };
-        })} selectedItem={mechanic} setSelectedItem={setMechanic} placeholder={"Select a mechanic"} title={null} />
-      <SelectWithSearch
-        items={categories.map((c) => {
-          return { id: c.id, name: c.name ?? "Unnamed mechanic" };
-        })} selectedItem={category} setSelectedItem={setCategory} placeholder={"Select a category"} title={null} />
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               onClick={() => {
                 mutationSearch.mutate(
