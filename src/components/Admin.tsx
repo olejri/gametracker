974d777927734520333adc0b305e17ab5997c55b
olejri <olejri@gmail.com>
2025-11-15 19:12:57 +0100

    Removed name of group in addmin

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -171,9 +171,6 @@ const AdminView = (props: {
         <div className="absolute inset-0 flex items-center" aria-hidden="true">
           <div className="w-full border-t border-gray-300" />
         </div>
-        <div className="relative flex justify-center">
-          <span className="bg-white px-2 text-sm text-gray-500">All users in {gameGroup}</span>
-        </div>
       </div>
       <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {allUsers.map((user) => (
