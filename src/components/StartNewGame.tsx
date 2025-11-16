4d9ee8f81e5ba0adeb59791552ac49a7534a2a3f
olejri <olejri@gmail.com>
2025-11-16 16:44:55 +0100

    Removed line

--- src/components/StartNewGame.tsx
+++ src/components/StartNewGame.tsx
@@ -174,14 +174,6 @@ const StartNewGame = (props: DashboardProps) => {
           </div>
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300" />
-        </div>
-        <div className="relative flex justify-center">
-          <span className="bg-white px-3 text-base font-semibold leading-6 text-gray-900">Games owned by {props.groupName}</span>
-        </div>
-      </div>
     </>
   );
 };
